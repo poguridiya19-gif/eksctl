ec346593f8cbf1e4adcdd6d91302fb7815cc4b8b
Poguri Diya <diya>
2026-04-03 15:21:39 +0530

    k8

--- workstation.sh
+++ workstation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-growpart /dev/nvme0n1 4
-lvextend -L +30G /dev/mapper/RootVG-varVol
-xfs_growfs /var
-dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+# growpart /dev/nvme0n1 4
+# lvextend -L +30G /dev/mapper/RootVG-varVol
+# xfs_growfs /var
+# dnf -y install dnf-plugins-core
+# dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+# dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# systemctl start docker
+# systemctl enable docker
+# usermod -aG docker ec2-user
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
